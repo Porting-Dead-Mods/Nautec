c6532068223c467f2ce41d018a3565e9f2705bac
thepigcat <benpospo@gmail.com>
2024-09-04 20:14:10 +0200

    basic power stuff

--- src/main/java/com/portingdeadmods/modjam/capabilities/power/IPowerStorage.java
+++ src/main/java/com/portingdeadmods/modjam/capabilities/power/IPowerStorage.java
@@ -1,4 +1,18 @@
 package com.portingdeadmods.modjam.capabilities.power;
 
+import org.jetbrains.annotations.Range;
+
 public interface IPowerStorage {
+    int getPowerStored();
+
+    int getPowerCapacity();
+
+    @Range(from = 0, to = 1)
+    float getPurity();
+
+    void setPowerStored(int powerStored);
+
+    void setPowerCapacity(int powerCapacity);
+
+    void setPurity(float purity);
 }
