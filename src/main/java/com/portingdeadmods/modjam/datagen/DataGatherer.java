7494eceb67a701309f1205c641ee98be6d97d626
Abdel <xdr.a123454321@gmail.com>
2024-09-04 20:48:23 +0200

    Datagen

--- src/main/java/com/portingdeadmods/modjam/datagen/DataGatherer.java
+++ src/main/java/com/portingdeadmods/modjam/datagen/DataGatherer.java
@@ -8,6 +8,7 @@ import net.minecraft.data.loot.LootTableProvider;
 import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.common.data.BlockTagsProvider;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import net.neoforged.neoforge.data.event.GatherDataEvent;
 
@@ -32,5 +33,7 @@ public class DataGatherer {
         generator.addProvider(event.includeServer(), new LootTableProvider(output, Collections.emptySet(), List.of(
                 new LootTableProvider.SubProviderEntry(MJLootTableProvider::new, LootContextParamSets.BLOCK)
         ), lookupProvider));
+        // generator.addProvider(event.includeClient(), new ItemTagProvider(output, lookupProvider,));
+        generator.addProvider(event.includeClient(), new BlockTagProvider(output, lookupProvider, ModJam.MODID, existingFileHelper));
     }
 }
