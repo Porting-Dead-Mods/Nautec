eadde8080551008507d864070ba9fac50a534847
Abdel <xdr.a123454321@gmail.com>
2024-09-05 10:16:05 +0200

    add back curios code

--- src/main/java/com/portingdeadmods/modjam/content/items/PrismMonocleItem.java
+++ src/main/java/com/portingdeadmods/modjam/content/items/PrismMonocleItem.java
@@ -3,10 +3,15 @@ package com.portingdeadmods.modjam.content.items;
 import com.portingdeadmods.modjam.api.items.IPowerItem;
 import com.portingdeadmods.modjam.data.MJDataComponents;
 import com.portingdeadmods.modjam.data.components.ComponentPowerStorage;
+import com.portingdeadmods.modjam.registries.MJItems;
 import com.portingdeadmods.modjam.tiers.MJArmorMaterials;
 import com.portingdeadmods.modjam.utils.ItemUtils;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
+import top.theillusivec4.curios.api.CuriosCapability;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurio;
 
 public class PrismMonocleItem extends ArmorItem implements IPowerItem {
     public PrismMonocleItem(Properties properties) {
@@ -15,6 +20,24 @@ public class PrismMonocleItem extends ArmorItem implements IPowerItem {
                 .stacksTo(1)
         );
     }
+
+    public static void registerCapabilities(final RegisterCapabilitiesEvent evt) {
+        evt.registerItem(
+                CuriosCapability.ITEM,
+                (stack, context) -> new ICurio() {
+
+                    @Override
+                    public ItemStack getStack() {
+                        return MJItems.PRISM_MONOCLE.toStack();
+                    }
+
+                    @Override
+                    public void curioTick(SlotContext slotContext) {
+                        // ticking logic here
+                    }
+                }, MJItems.PRISM_MONOCLE.get());
+    }
+
 
     @Override
     public boolean isBarVisible(ItemStack stack) {
