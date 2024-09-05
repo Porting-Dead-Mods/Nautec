67008a15ae460dccff3932fdd0cfa34341b8124a
Abdel <xdr.a123454321@gmail.com>
2024-09-05 10:11:52 +0200

    Added curios support

--- src/main/java/com/portingdeadmods/modjam/client/hud/PrismMonocleOverlay.java
+++ src/main/java/com/portingdeadmods/modjam/client/hud/PrismMonocleOverlay.java
@@ -2,6 +2,7 @@ package com.portingdeadmods.modjam.client.hud;
 
 import com.portingdeadmods.modjam.api.blocks.DisplayBlock;
 import com.portingdeadmods.modjam.content.items.PrismMonocleItem;
+import com.portingdeadmods.modjam.registries.MJItems;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.LayeredDraw;
 import net.minecraft.core.BlockPos;
@@ -11,6 +12,10 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.BlockHitResult;
+import top.theillusivec4.curios.api.CuriosApi;
+import top.theillusivec4.curios.api.SlotResult;
+
+import java.util.Optional;
 
 public final class PrismMonocleOverlay {
     public static final LayeredDraw.Layer HUD = (guiGraphics, delta) -> {
@@ -21,7 +26,9 @@ public final class PrismMonocleOverlay {
         Level level = mc.level;
         Player player = mc.player;
         ItemStack item = player.getItemBySlot(EquipmentSlot.HEAD);
-        if (item.getItem() instanceof PrismMonocleItem && mc.hitResult instanceof BlockHitResult blockHitResult) {
+        Optional<SlotResult> slotResult = CuriosApi.getCuriosInventory(player).flatMap(handler -> handler.findFirstCurio(MJItems.PRISM_MONOCLE.get()));
+
+        if ((item.getItem() instanceof PrismMonocleItem || slotResult.isPresent()) && mc.hitResult instanceof BlockHitResult blockHitResult) {
             BlockPos blockPos = blockHitResult.getBlockPos();
             if (level.getBlockState(blockPos).getBlock() instanceof DisplayBlock displayBlock && displayBlock.display(level, blockPos, player)) {
                 for (Component component : displayBlock.displayText(level, blockPos, player)) {
--- src/main/java/com/portingdeadmods/modjam/content/items/PrismMonocleItem.java
+++ src/main/java/com/portingdeadmods/modjam/content/items/PrismMonocleItem.java
@@ -3,11 +3,16 @@ package com.portingdeadmods.modjam.content.items;
 import com.portingdeadmods.modjam.api.items.IPowerItem;
 import com.portingdeadmods.modjam.data.MJDataComponents;
 import com.portingdeadmods.modjam.data.components.ComponentPowerStorage;
+import com.portingdeadmods.modjam.registries.MJItems;
 import com.portingdeadmods.modjam.utils.ItemUtils;
 import net.minecraft.core.Holder;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
+import top.theillusivec4.curios.api.CuriosCapability;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurio;
 
 public class PrismMonocleItem extends ArmorItem implements IPowerItem {
     public PrismMonocleItem(Holder<ArmorMaterial> material, Properties properties) {
@@ -15,6 +20,35 @@ public class PrismMonocleItem extends ArmorItem implements IPowerItem {
                 .durability(0)
                 .stacksTo(1)
         );
+    }
+
+    public static void registerCapabilities(final RegisterCapabilitiesEvent evt) {
+        evt.registerItem(
+                CuriosCapability.ITEM,
+                (stack, context) -> new ICurio() {
+
+                    @Override
+                    public ItemStack getStack() {
+                        return MJItems.PRISM_MONOCLE.toStack();
+                    }
+
+                    @Override
+                    public void onEquip(SlotContext slotContext, ItemStack prevStack) {
+                        ICurio.super.onEquip(slotContext, prevStack);
+                    }
+
+                    @Override
+                    public void onUnequip(SlotContext slotContext, ItemStack newStack) {
+                        ICurio.super.onUnequip(slotContext, newStack);
+                    }
+
+                    @Override
+                    public void curioTick(SlotContext slotContext) {
+                        // ticking logic here
+                    }
+
+
+                }, MJItems.PRISM_MONOCLE.get());
     }
 
     @Override
