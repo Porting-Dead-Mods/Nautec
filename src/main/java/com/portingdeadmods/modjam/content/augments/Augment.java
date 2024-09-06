a438a6b3379cbd3368f759065b53d7f576a73061
ktpatient <167013520+ktpatient@users.noreply.github.com>
2024-09-06 23:16:33 +0100

    Add events for augments

--- src/main/java/com/portingdeadmods/modjam/content/augments/Augment.java
+++ src/main/java/com/portingdeadmods/modjam/content/augments/Augment.java
@@ -1,12 +1,21 @@
 package com.portingdeadmods.modjam.content.augments;
 
 import net.minecraft.world.entity.player.Player;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 
 @SuppressWarnings("unused")
 public class Augment {
     public static void onBreak(BlockEvent.BreakEvent event){
         Player player = event.getPlayer();
+        event.setCanceled(true);
+    }
+    public static void onBreakSpeed(PlayerEvent.BreakSpeed event){
+        event.setNewSpeed(0.0f);
+        event.setCanceled(true);
+    }
+    public static void onBlockLeftClick(PlayerInteractEvent.LeftClickBlock event){
         event.setCanceled(true);
     }
 }
