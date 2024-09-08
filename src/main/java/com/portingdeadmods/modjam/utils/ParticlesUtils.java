26fa83af8029f27ac6f3b92608e308798da789eb
thepigcat <benpospo@gmail.com>
2024-09-08 11:32:29 +0200

    fix lighting

--- src/main/java/com/portingdeadmods/modjam/utils/ParticlesUtils.java
+++ src/main/java/com/portingdeadmods/modjam/utils/ParticlesUtils.java
@@ -11,7 +11,7 @@ public class ParticlesUtils {
     private static final double PARTICLE_RADIUS = 0.5; // Radius of the sphere around the item
     private static final int PARTICLE_DELAY = 5; // Number of ticks between particle spawns
 
-    private int particleTicks = 0;
+    private static int particleTicks = 0;
 
     public static void spawnParticles(ItemEntity itemEntity, Level level, ParticleOptions particlesTypes) {
         // Spawn particles around the item entity in a spherical pattern
