0c29c8652cf8dafd28b4fbe1282bc2bb1ff81617
Abdel <xdr.a123454321@gmail.com>
2024-09-08 17:51:06 +0200

    fix JEI

--- src/main/java/com/portingdeadmods/modjam/compat/jei/ItemTransformationRecipeCategory.java
+++ src/main/java/com/portingdeadmods/modjam/compat/jei/ItemTransformationRecipeCategory.java
@@ -3,12 +3,9 @@ package com.portingdeadmods.modjam.compat.jei;
 import com.portingdeadmods.modjam.ModJam;
 import com.portingdeadmods.modjam.recipes.ItemTransformationRecipe;
 import com.portingdeadmods.modjam.registries.MJBlocks;
-import mezz.jei.api.constants.ModIds;
 import mezz.jei.api.constants.VanillaTypes;
 import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
 import mezz.jei.api.gui.drawable.IDrawable;
-import mezz.jei.api.gui.drawable.IDrawableAnimated;
-import mezz.jei.api.gui.widgets.IRecipeExtrasBuilder;
 import mezz.jei.api.helpers.IGuiHelper;
 import mezz.jei.api.recipe.IFocusGroup;
 import mezz.jei.api.recipe.RecipeIngredientRole;
@@ -22,24 +19,14 @@ import org.jetbrains.annotations.Nullable;
 public class ItemTransformationRecipeCategory implements IRecipeCategory<ItemTransformationRecipe> {
 
     public static final ResourceLocation UID = ResourceLocation.fromNamespaceAndPath(ModJam.MODID, "item_transformation");
-
-    public static final String TEXTURE_GUI_PATH = "textures/jei/gui/";
-
-    public static final String TEXTURE_GUI_VANILLA = TEXTURE_GUI_PATH + "gui_vanilla.png";
-
-    public static final ResourceLocation RECIPE_GUI_VANILLA = ResourceLocation.fromNamespaceAndPath(ModIds.JEI_ID, TEXTURE_GUI_VANILLA);
-
     public static final RecipeType<ItemTransformationRecipe> RECIPE_TYPE =
             new RecipeType<>(UID, ItemTransformationRecipe.class);
 
     private final IDrawable icon;
-    private final IDrawableAnimated arrow;
     private final IDrawable background;
 
 
     public ItemTransformationRecipeCategory(IGuiHelper helper) {
-        this.arrow = helper.drawableBuilder(RECIPE_GUI_VANILLA, 82, 128, 24, 17)
-                .buildAnimated(0, IDrawableAnimated.StartDirection.LEFT, false);
         this.background = helper.createBlankDrawable(80, 16);
         this.icon = helper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(MJBlocks.AQUATIC_CATALYST.get()));
     }
@@ -69,10 +56,5 @@ public class ItemTransformationRecipeCategory implements IRecipeCategory<ItemTra
         //Just one input slot, an arrow and an output slot
         builder.addSlot(RecipeIngredientRole.INPUT, 0,0 ).addItemStack(recipe.getIngredients().get(0).getItems()[0]);
         builder.addSlot(RecipeIngredientRole.OUTPUT, 64, 0).addItemStack(recipe.getResultItem(null));
-    }
-
-    @Override
-    public void createRecipeExtras(IRecipeExtrasBuilder builder, ItemTransformationRecipe recipe, IFocusGroup focuses) {
-        // FUCK JEI FUCK JEI FUCK JEI
     }
 }
