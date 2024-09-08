b20fdafea3d6e0086432fffd99865edd3e528f64
Abdel <xdr.a123454321@gmail.com>
2024-09-08 17:47:22 +0200

    finished JEI integration (besides the arrow)

---
+++ src/main/java/com/portingdeadmods/modjam/compat/jei/MJJeiPlugin.java
@@ -0,0 +1,38 @@
+package com.portingdeadmods.modjam.compat.jei;
+
+import com.portingdeadmods.modjam.ModJam;
+import com.portingdeadmods.modjam.recipes.ItemTransformationRecipe;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.crafting.RecipeHolder;
+import net.minecraft.world.item.crafting.RecipeManager;
+
+import java.util.List;
+
+@JeiPlugin
+public class MJJeiPlugin implements IModPlugin {
+
+    @Override
+    public ResourceLocation getPluginUid() {
+        return ResourceLocation.fromNamespaceAndPath(ModJam.MODID,"jei_plugin");
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        registration.addRecipeCategories(new ItemTransformationRecipeCategory(
+                registration.getJeiHelpers().getGuiHelper()));
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        RecipeManager recipeManager = Minecraft.getInstance().level.getRecipeManager();
+
+        List<ItemTransformationRecipe> transformationRecipes = recipeManager.getAllRecipesFor(ItemTransformationRecipe.Type.INSTANCE)
+                .stream().map(RecipeHolder::value).toList();
+        registration.addRecipes(ItemTransformationRecipeCategory.RECIPE_TYPE, transformationRecipes);
+    }
+}
