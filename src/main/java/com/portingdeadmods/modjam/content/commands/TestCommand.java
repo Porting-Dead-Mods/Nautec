a473a4f12930c836ce14f3e0f834a783265ff374
thepigcat <benpospo@gmail.com>
2024-09-09 14:36:25 +0200

    commands

---
+++ src/main/java/com/portingdeadmods/modjam/content/commands/TestCommand.java
@@ -0,0 +1,30 @@
+package com.portingdeadmods.modjam.content.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.context.CommandContext;
+import com.portingdeadmods.modjam.ModJam;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.OutgoingChatMessage;
+import net.minecraft.network.chat.PlayerChatMessage;
+import net.minecraft.server.level.ServerLevel;
+
+public class TestCommand {
+    public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
+        dispatcher.register(Commands.literal(ModJam.MODID)
+                .then(Commands.literal("test")
+                        .then(Commands.literal("stuff")
+                                .executes(TestCommand::test))
+                )
+        );
+    }
+
+    private static int test(CommandContext<CommandSourceStack> ctx) {
+        CommandSourceStack source = ctx.getSource();
+        ServerLevel level = source.getLevel();
+        source.sendChatMessage(OutgoingChatMessage.create(PlayerChatMessage.system("Level: " + level)), false, ChatType.bind(ChatType.CHAT, source));
+        return 1;
+    }
+}
