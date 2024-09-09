3c3ec85d60475f3c59b701e4a2d822f77f398e54
ktpatient <167013520+ktpatient@users.noreply.github.com>
2024-09-09 17:18:30 +0100

    EmptyAugment

--- src/main/java/com/portingdeadmods/modjam/registries/MJAugments.java
+++ src/main/java/com/portingdeadmods/modjam/registries/MJAugments.java
@@ -2,10 +2,7 @@ package com.portingdeadmods.modjam.registries;
 
 import com.portingdeadmods.modjam.MJRegistries;
 import com.portingdeadmods.modjam.ModJam;
-import com.portingdeadmods.modjam.content.augments.DisallowBreakingAugment;
-import com.portingdeadmods.modjam.content.augments.GiveDiamondAugment;
-import com.portingdeadmods.modjam.content.augments.StaticAugment;
-import com.portingdeadmods.modjam.content.augments.ThrowSnowballAugment;
+import com.portingdeadmods.modjam.content.augments.*;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import java.util.function.Supplier;
@@ -13,6 +10,8 @@ import java.util.function.Supplier;
 public final class MJAugments {
     public static final DeferredRegister<StaticAugment> AUGMENTS = DeferredRegister.create(MJRegistries.AUGMENT, ModJam.MODID);
 
+
+    public static final Supplier<EmptyAugment> EMPTY_AUGMENT = AUGMENTS.register("empty_augment", EmptyAugment::new);
     public static final Supplier<GiveDiamondAugment> GIVE_DIAMOND = AUGMENTS.register("give_diamond", GiveDiamondAugment::new);
     public static final Supplier<DisallowBreakingAugment> DISALLOW_BREAKING = AUGMENTS.register("disallow_breaking", DisallowBreakingAugment::new);
     public static final Supplier<ThrowSnowballAugment> THROW_SNOWBALL = AUGMENTS.register("throw_snowball", ThrowSnowballAugment::new);
