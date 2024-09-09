ff02f81d56ee48e574657c699e9b47098f06a7f1
ktpatient <167013520+ktpatient@users.noreply.github.com>
2024-09-09 13:16:36 +0100

    Remove Payload actions

--- src/main/java/com/portingdeadmods/modjam/network/SetCooldownPayload.java
+++ src/main/java/com/portingdeadmods/modjam/network/SetCooldownPayload.java
@@ -1,11 +1,14 @@
 package com.portingdeadmods.modjam.network;
 
 import com.portingdeadmods.modjam.ModJam;
+import com.portingdeadmods.modjam.capabilities.augmentation.Slot;
+import com.portingdeadmods.modjam.content.augments.AugmentHelper;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.neoforge.network.handling.IPayloadContext;
 import org.jetbrains.annotations.NotNull;
 
 public record SetCooldownPayload (int cooldown, int slot)implements CustomPacketPayload {
@@ -21,5 +24,11 @@ public record SetCooldownPayload (int cooldown, int slot)implements CustomPacket
     @Override
     public @NotNull Type<? extends CustomPacketPayload> type() {
         return TYPE;
+    }
+
+    public static void setCooldownAction(SetCooldownPayload payload, IPayloadContext context){
+        context.enqueueWork(()->{
+            AugmentHelper.setCooldown(context.player(), Slot.GetValue(payload.slot()), payload.cooldown());
+        });
     }
 }
