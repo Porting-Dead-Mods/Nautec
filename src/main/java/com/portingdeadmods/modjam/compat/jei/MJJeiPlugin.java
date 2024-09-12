b7cb6c0355633405b778691a86f624345d04122c
Abdel <xdr.a123454321@gmail.com>
2024-09-12 21:47:18 +0200

    implemented a fully working etching system

--- src/main/java/com/portingdeadmods/modjam/compat/jei/MJJeiPlugin.java
+++ src/main/java/com/portingdeadmods/modjam/compat/jei/MJJeiPlugin.java
@@ -2,8 +2,10 @@ package com.portingdeadmods.modjam.compat.jei;
 
 import com.portingdeadmods.modjam.ModJam;
 import com.portingdeadmods.modjam.content.recipes.AquaticCatalystChannelingRecipe;
+import com.portingdeadmods.modjam.content.recipes.ItemEtchingRecipe;
 import com.portingdeadmods.modjam.content.recipes.ItemTransformationRecipe;
 import com.portingdeadmods.modjam.registries.MJBlocks;
+import com.portingdeadmods.modjam.registries.MJItems;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.registration.IRecipeCatalystRegistration;
@@ -33,6 +35,9 @@ public class MJJeiPlugin implements IModPlugin {
 
         registration.addRecipeCategories(new AquaticCatalystChannelingRecipeCategory(
                 registration.getJeiHelpers().getGuiHelper()));
+
+        registration.addRecipeCategories(new ItemEtchingRecipeCategory(
+                registration.getJeiHelpers().getGuiHelper()));
     }
 
     @Override
@@ -45,13 +50,19 @@ public class MJJeiPlugin implements IModPlugin {
         List<AquaticCatalystChannelingRecipe> channelingRecipes = recipeManager.getAllRecipesFor(AquaticCatalystChannelingRecipe.Type.INSTANCE)
                 .stream().map(RecipeHolder::value).toList();
 
+        List<ItemEtchingRecipe> etchingRecipes = recipeManager.getAllRecipesFor(ItemEtchingRecipe.Type.INSTANCE)
+                .stream().map(RecipeHolder::value).toList();
+
         registration.addRecipes(ItemTransformationRecipeCategory.RECIPE_TYPE, transformationRecipes);
         registration.addRecipes(AquaticCatalystChannelingRecipeCategory.RECIPE_TYPE, channelingRecipes);
+        registration.addRecipes(ItemEtchingRecipeCategory.RECIPE_TYPE, etchingRecipes);
     }
 
     @Override
     public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
         registration.addRecipeCatalyst(new ItemStack(MJBlocks.AQUATIC_CATALYST.get()),
                 AquaticCatalystChannelingRecipeCategory.RECIPE_TYPE);
+        registration.addRecipeCatalyst(new ItemStack(MJItems.ETCHING_ACID_BUCKET.get()),
+                ItemEtchingRecipeCategory.RECIPE_TYPE);
     }
 }
