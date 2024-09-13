f5428c56ef6f8fa276b12b20309afe12ccbcbc6b
thepigcat <benpospo@gmail.com>
2024-09-13 22:33:33 +0200

    diving armor item texture

--- src/main/java/com/portingdeadmods/modjam/client/renderer/blockentities/MixerBERenderer.java
+++ src/main/java/com/portingdeadmods/modjam/client/renderer/blockentities/MixerBERenderer.java
@@ -1,28 +1,38 @@
 package com.portingdeadmods.modjam.client.renderer.blockentities;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 import com.portingdeadmods.modjam.api.client.renderer.blockentities.LaserBlockEntityRenderer;
 import com.portingdeadmods.modjam.api.utils.HorizontalDirection;
 import com.portingdeadmods.modjam.client.model.WhiskModel;
 import com.portingdeadmods.modjam.content.blockentities.MixerBlockEntity;
+import com.portingdeadmods.modjam.content.blocks.MixerBlock;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
 import net.minecraft.client.renderer.entity.ItemRenderer;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.core.Vec3i;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LightLayer;
+import net.neoforged.neoforge.client.extensions.common.IClientFluidTypeExtensions;
+import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.fluids.capability.IFluidHandler;
 import net.neoforged.neoforge.items.IItemHandler;
+import org.joml.Matrix4f;
 import org.joml.Vector3f;
 
 public class MixerBERenderer extends LaserBlockEntityRenderer<MixerBlockEntity> {
+    private static final float SIDE_MARGIN = (float) MixerBlock.SHAPE.min(Direction.Axis.X) + 0.125f;
+    private static final float MIN_Y = 2 / 16f;
+    private static final float MAX_Y = 1 - MIN_Y;
     private final WhiskModel model;
 
     public MixerBERenderer(BlockEntityRendererProvider.Context ctx) {
@@ -48,10 +58,13 @@ public class MixerBERenderer extends LaserBlockEntityRenderer<MixerBlockEntity> 
             );
         }
         poseStack.popPose();
+
         IItemHandler handler = blockEntity.getItemHandler();
         for (int i = 0; i < handler.getSlots() - 1; i++) {
             renderItem(handler.getStackInSlot(i), blockEntity, i, poseStack, bufferSource, packedOverlay);
         }
+
+        renderFluid(blockEntity, poseStack, bufferSource, packedLight);
     }
 
     private static void renderItem(ItemStack itemStack, MixerBlockEntity mixerBE, int index, PoseStack poseStack, MultiBufferSource bufferSource, int packedOverlay) {
@@ -84,5 +97,68 @@ public class MixerBERenderer extends LaserBlockEntityRenderer<MixerBlockEntity> 
         int blockLight = level.getBrightness(LightLayer.BLOCK, pos);
         int skyLight = level.getBrightness(LightLayer.SKY, pos);
         return LightTexture.pack(skyLight, blockLight);
+    }
+
+    private static void renderFluid(MixerBlockEntity blockEntity, PoseStack poseStack, MultiBufferSource pBufferSource, int combinedLight) {
+        IFluidHandler fluidHandler = blockEntity.getFluidHandler();
+        FluidStack fluidStack = fluidHandler.getFluidInTank(0);
+        int fluidCapacity = fluidHandler.getTankCapacity(0);
+        if (fluidStack.isEmpty())
+            return;
+
+        float fillPercentage = Math.min(1, (float) fluidStack.getAmount() / fluidCapacity) / 2;
+        if (fluidStack.getFluid().getFluidType().isLighterThanAir())
+            renderFluid(poseStack, pBufferSource, fluidStack, fillPercentage, 1, combinedLight);
+        else
+            renderFluid(poseStack, pBufferSource, fluidStack, 1, fillPercentage, combinedLight);
+    }
+
+    private static void renderFluid(PoseStack poseStack, MultiBufferSource bufferSource, FluidStack fluidStack, float alpha, float heightPercentage, int combinedLight) {
+        VertexConsumer vertexBuilder = bufferSource.getBuffer(RenderType.translucent());
+        IClientFluidTypeExtensions fluidTypeExtensions = IClientFluidTypeExtensions.of(fluidStack.getFluid());
+        //noinspection deprecation
+        TextureAtlasSprite sprite = Minecraft.getInstance().getTextureAtlas(TextureAtlas.LOCATION_BLOCKS).apply(fluidTypeExtensions.getStillTexture(fluidStack));
+        int color = fluidTypeExtensions.getTintColor();
+        alpha *= (color >> 24 & 255) / 255f;
+        float red = (color >> 16 & 255) / 255f;
+        float green = (color >> 8 & 255) / 255f;
+        float blue = (color & 255) / 255f;
+
+        renderQuads(poseStack.last().pose(), vertexBuilder, sprite, red, green, blue, alpha, heightPercentage, combinedLight);
+    }
+
+    private static void renderQuads(Matrix4f matrix, VertexConsumer buffer, TextureAtlasSprite sprite, float r, float g, float b, float alpha, float heightPercentage, int light) {
+        float height = MIN_Y + (MAX_Y - MIN_Y) * heightPercentage;
+        float minU = sprite.getU(SIDE_MARGIN), maxU = sprite.getU((1 - SIDE_MARGIN));
+        float minV = sprite.getV(MIN_Y), maxV = sprite.getV(height);
+        // min z
+        buffer.addVertex(matrix, SIDE_MARGIN, MIN_Y, SIDE_MARGIN).setColor(r, g, b, alpha).setUv(minU, minV).setLight(light).setNormal(0, 0, -1);
+        buffer.addVertex(matrix, SIDE_MARGIN, height, SIDE_MARGIN).setColor(r, g, b, alpha).setUv(minU, maxV).setLight(light).setNormal(0, 0, -1);
+        buffer.addVertex(matrix, 1 - SIDE_MARGIN, height, SIDE_MARGIN).setColor(r, g, b, alpha).setUv(maxU, maxV).setLight(light).setNormal(0, 0, -1);
+        buffer.addVertex(matrix, 1 - SIDE_MARGIN, MIN_Y, SIDE_MARGIN).setColor(r, g, b, alpha).setUv(maxU, minV).setLight(light).setNormal(0, 0, -1);
+        // max z
+        buffer.addVertex(matrix, SIDE_MARGIN, MIN_Y, 1 - SIDE_MARGIN).setColor(r, g, b, alpha).setUv(minU, minV).setLight(light).setNormal(0, 0, 1);
+        buffer.addVertex(matrix, 1 - SIDE_MARGIN, MIN_Y, 1 - SIDE_MARGIN).setColor(r, g, b, alpha).setUv(maxU, minV).setLight(light).setNormal(0, 0, 1);
+        buffer.addVertex(matrix, 1 - SIDE_MARGIN, height, 1 - SIDE_MARGIN).setColor(r, g, b, alpha).setUv(maxU, maxV).setLight(light).setNormal(0, 0, 1);
+        buffer.addVertex(matrix, SIDE_MARGIN, height, 1 - SIDE_MARGIN).setColor(r, g, b, alpha).setUv(minU, maxV).setLight(light).setNormal(0, 0, 1);
+        // min x
+        buffer.addVertex(matrix, SIDE_MARGIN, MIN_Y, SIDE_MARGIN).setColor(r, g, b, alpha).setUv(minU, minV).setLight(light).setNormal(-1, 0, 0);
+        buffer.addVertex(matrix, SIDE_MARGIN, MIN_Y, 1 - SIDE_MARGIN).setColor(r, g, b, alpha).setUv(maxU, minV).setLight(light).setNormal(-1, 0, 0);
+        buffer.addVertex(matrix, SIDE_MARGIN, height, 1 - SIDE_MARGIN).setColor(r, g, b, alpha).setUv(maxU, maxV).setLight(light).setNormal(-1, 0, 0);
+        buffer.addVertex(matrix, SIDE_MARGIN, height, SIDE_MARGIN).setColor(r, g, b, alpha).setUv(minU, maxV).setLight(light).setNormal(-1, 0, 0);
+        // max x
+        buffer.addVertex(matrix, 1 - SIDE_MARGIN, MIN_Y, SIDE_MARGIN).setColor(r, g, b, alpha).setUv(minU, minV).setLight(light).setNormal(1, 0, 0);
+        buffer.addVertex(matrix, 1 - SIDE_MARGIN, height, SIDE_MARGIN).setColor(r, g, b, alpha).setUv(minU, maxV).setLight(light).setNormal(1, 0, 0);
+        buffer.addVertex(matrix, 1 - SIDE_MARGIN, height, 1 - SIDE_MARGIN).setColor(r, g, b, alpha).setUv(maxU, maxV).setLight(light).setNormal(1, 0, 0);
+        buffer.addVertex(matrix, 1 - SIDE_MARGIN, MIN_Y, 1 - SIDE_MARGIN).setColor(r, g, b, alpha).setUv(maxU, minV).setLight(light).setNormal(1, 0, 0);
+        // top
+        if (heightPercentage < 1) {
+            minV = sprite.getV(SIDE_MARGIN);
+            maxV = sprite.getV(1 - SIDE_MARGIN);
+            buffer.addVertex(matrix, SIDE_MARGIN, height, SIDE_MARGIN).setColor(r, g, b, alpha).setUv(minU, minV).setLight(light).setNormal(0, 1, 0);
+            buffer.addVertex(matrix, SIDE_MARGIN, height, 1 - SIDE_MARGIN).setColor(r, g, b, alpha).setUv(minU, maxV).setLight(light).setNormal(0, 1, 0);
+            buffer.addVertex(matrix, 1 - SIDE_MARGIN, height, 1 - SIDE_MARGIN).setColor(r, g, b, alpha).setUv(maxU, maxV).setLight(light).setNormal(0, 1, 0);
+            buffer.addVertex(matrix, 1 - SIDE_MARGIN, height, SIDE_MARGIN).setColor(r, g, b, alpha).setUv(maxU, minV).setLight(light).setNormal(0, 1, 0);
+        }
     }
 }
