9493e9999c66985492c95064ab2f6fcdbe77df9a
Abdel <xdr.a123454321@gmail.com>
2024-09-19 21:09:36 +0200

    shit ass code for tools

--- src/main/java/com/portingdeadmods/modjam/content/items/tools/AquarineShovelItem.java
+++ src/main/java/com/portingdeadmods/modjam/content/items/tools/AquarineShovelItem.java
@@ -7,13 +7,15 @@ import com.portingdeadmods.modjam.content.items.tiers.MJToolMaterials;
 import com.portingdeadmods.modjam.data.MJDataComponents;
 import com.portingdeadmods.modjam.data.components.ComponentPowerStorage;
 import com.portingdeadmods.modjam.utils.ItemUtils;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
+import net.minecraft.ChatFormatting;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ShovelItem;
-import net.minecraft.world.level.Level;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
+
+import java.util.List;
 
 public class AquarineShovelItem extends ShovelItem implements IPowerItem {
     public AquarineShovelItem() {
@@ -21,16 +23,17 @@ public class AquarineShovelItem extends ShovelItem implements IPowerItem {
                 .component(MJDataComponents.POWER, ComponentPowerStorage.withCapacity(700)));
     }
 
+
     @Override
-    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand usedHand) {
-        if (!level.isClientSide) {
-            ItemStack stack = player.getItemInHand(usedHand);
-            IPowerStorage powerStorage = stack.getCapability(MJCapabilities.PowerStorage.ITEM);
-            if(powerStorage.getPowerStored() <= 0) {
-                return InteractionResultHolder.fail(stack);
-            }
+    public InteractionResult useOn(UseOnContext context) {
+        ItemStack stack = context.getItemInHand();
+        IPowerStorage powerStorage = stack.getCapability(MJCapabilities.PowerStorage.ITEM);
+        if(powerStorage.getPowerStored() <= 0) {
+            return InteractionResult.FAIL;
+        }else {
+            powerStorage.tryDrainPower(1, false);
         }
-        return InteractionResultHolder.success(player.getItemInHand(usedHand));
+        return InteractionResult.SUCCESS;
     }
 
     @Override
@@ -66,5 +69,12 @@ public class AquarineShovelItem extends ShovelItem implements IPowerItem {
     @Override
     public int getMaxOutput() {
         return 0;
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+        super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+        IPowerStorage powerStorage = stack.getCapability(MJCapabilities.PowerStorage.ITEM);
+        tooltipComponents.add(Component.literal("Power: " + powerStorage.getPowerStored() + "/" + powerStorage.getPowerCapacity()).withStyle(ChatFormatting.DARK_AQUA));
     }
 }
