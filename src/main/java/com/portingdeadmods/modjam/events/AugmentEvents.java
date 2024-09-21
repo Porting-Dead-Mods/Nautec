95bea2a5b01ac95aa2b328e965483415300dce83
thepigcat <benpospo@gmail.com>
2024-09-21 18:23:46 +0200

    fixed game crash when dying with augments

--- src/main/java/com/portingdeadmods/modjam/events/AugmentEvents.java
+++ src/main/java/com/portingdeadmods/modjam/events/AugmentEvents.java
@@ -4,21 +4,27 @@ import com.portingdeadmods.modjam.ModJam;
 import com.portingdeadmods.modjam.api.augments.Augment;
 import com.portingdeadmods.modjam.api.augments.AugmentSlot;
 import com.portingdeadmods.modjam.content.augments.AugmentSlots;
+import com.portingdeadmods.modjam.network.SyncAugmentPayload;
 import com.portingdeadmods.modjam.utils.AugmentHelper;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.player.PlayerEvent;
 import net.neoforged.neoforge.event.level.BlockEvent;
 import net.neoforged.neoforge.event.tick.PlayerTickEvent;
+import net.neoforged.neoforge.network.PacketDistributor;
 
 import java.util.List;
+import java.util.Map;
 
 @SuppressWarnings("unused")
 @EventBusSubscriber(modid = ModJam.MODID)
 public final class AugmentEvents {
 
     @SubscribeEvent
-    public static void breakEvent(BlockEvent.BreakEvent event){
+    public static void breakEvent(BlockEvent.BreakEvent event) {
         Iterable<Augment> augments = AugmentHelper.getAugments(event.getPlayer()).values();
         for (Augment augment : augments) {
             if (augment != null) {
@@ -30,13 +36,28 @@ public final class AugmentEvents {
 
 
     @SubscribeEvent
-    public static void playerTick(PlayerTickEvent.Post event){
-        Iterable<Augment> augments = AugmentHelper.getAugments(event.getEntity()).values();
+    public static void playerTick(PlayerTickEvent.Post event) {
+        Player player = event.getEntity();
+        Iterable<Augment> augments = AugmentHelper.getAugments(player).values();
         for (Augment augment : augments) {
             if (augment != null) {
                 AugmentSlot slot = augment.getAugmentSlot();
-                Player player = event.getEntity();
                 augment.commonTick(event);
+            }
+        }
+    }
+
+    @SubscribeEvent
+    public static void onPlayerRespawn(PlayerEvent.PlayerRespawnEvent event) {
+        Player player = event.getEntity();
+        Iterable<Augment> augments = AugmentHelper.getAugments(player).values();
+        Map<AugmentSlot, CompoundTag> data = AugmentHelper.getAugmentsData(player);
+        for (Augment augment : augments) {
+            if (augment != null) {
+                AugmentSlot slot = augment.getAugmentSlot();
+                augment.setPlayer(player);
+                CompoundTag nbt = data.get(slot);
+                PacketDistributor.sendToPlayer((ServerPlayer) player, new SyncAugmentPayload(augment, nbt != null ? nbt : new CompoundTag()));
             }
         }
     }
