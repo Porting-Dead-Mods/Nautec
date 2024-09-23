0fc867c93f8112ec524192eb603c0888dbeb442b
ktpatient <167013520+ktpatient@users.noreply.github.com>
2024-09-23 23:24:45 +0100

    Add Underwater mining speed Augment

---
+++ src/main/java/com/portingdeadmods/nautec/content/augments/UnderwaterMiningSpeed.java
@@ -0,0 +1,18 @@
+package com.portingdeadmods.nautec.content.augments;
+
+import com.portingdeadmods.nautec.api.augments.Augment;
+import com.portingdeadmods.nautec.api.augments.AugmentSlot;
+import com.portingdeadmods.nautec.api.augments.AugmentType;
+import com.portingdeadmods.nautec.registries.NTAugments;
+import org.jetbrains.annotations.Nullable;
+
+public class UnderwaterMiningSpeed extends Augment {
+    public UnderwaterMiningSpeed(AugmentSlot augmentSlot) {
+        super(NTAugments.UNDERWATER_MINING_SPEED_AUGMENT.get(), augmentSlot);
+    }
+
+    @Override
+    public @Nullable AugmentSlot[] getCompatibleSlots() {
+        return new AugmentSlot[0];
+    }
+}
