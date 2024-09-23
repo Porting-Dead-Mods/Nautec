89b90e099860c2b34398235a8d7bfc6c273884a8
ktpatient <167013520+ktpatient@users.noreply.github.com>
2024-09-23 23:30:32 +0100

    Add BonusHearts Augment

---
+++ src/main/java/com/portingdeadmods/nautec/content/augments/BonusHeartsAugment.java
@@ -0,0 +1,18 @@
+package com.portingdeadmods.nautec.content.augments;
+
+import com.portingdeadmods.nautec.api.augments.Augment;
+import com.portingdeadmods.nautec.api.augments.AugmentSlot;
+import com.portingdeadmods.nautec.api.augments.AugmentType;
+import com.portingdeadmods.nautec.registries.NTAugments;
+import org.jetbrains.annotations.Nullable;
+
+public class BonusHeartsAugment extends Augment {
+    public BonusHeartsAugment(AugmentSlot augmentSlot) {
+        super(NTAugments.BONUS_HEART_AUGMENT.get(), augmentSlot);
+    }
+
+    @Override
+    public @Nullable AugmentSlot[] getCompatibleSlots() {
+        return new AugmentSlot[0];
+    }
+}
