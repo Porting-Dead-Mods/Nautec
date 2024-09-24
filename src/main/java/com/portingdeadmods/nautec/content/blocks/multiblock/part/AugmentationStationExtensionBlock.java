f097ab45d5f33152785b07e73b615fc85f975146
iglée42 <igleev2@gmail.com>
2024-09-24 17:50:40 +0200

    Add shapes for Augmentation Station

--- src/main/java/com/portingdeadmods/nautec/content/blocks/multiblock/part/AugmentationStationExtensionBlock.java
+++ src/main/java/com/portingdeadmods/nautec/content/blocks/multiblock/part/AugmentationStationExtensionBlock.java
@@ -9,6 +9,7 @@ import com.portingdeadmods.nautec.registries.NTBlockEntityTypes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.Block;
@@ -17,6 +18,9 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.NotNull;
 
 public class AugmentationStationExtensionBlock extends LaserBlock {
@@ -44,5 +48,10 @@ public class AugmentationStationExtensionBlock extends LaserBlock {
     protected @NotNull InteractionResult useWithoutItem(BlockState p_60503_, Level p_60504_, BlockPos p_60505_, Player p_60506_, BlockHitResult p_60508_) {
         p_60506_.openMenu((AugmentationStationExtensionBlockEntity) p_60504_.getBlockEntity(p_60505_), p_60505_);
         return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return Shapes.box(0, 0, 0, 1, 0.625, 1);
     }
 }
