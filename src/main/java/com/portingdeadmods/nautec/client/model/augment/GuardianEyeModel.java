ed525b581d0f4be82b0cb81998992a2613e30ee5
thepigcat <benpospo@gmail.com>
2024-09-24 21:53:08 +0200

    start working on guardian eye

---
+++ src/main/java/com/portingdeadmods/nautec/client/model/augment/GuardianEyeModel.java
@@ -0,0 +1,50 @@
+package com.portingdeadmods.nautec.client.model.augment;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.portingdeadmods.nautec.Nautec;
+import com.portingdeadmods.nautec.api.client.model.augments.AugmentModel;
+import com.portingdeadmods.nautec.content.augments.GuardianEyeAugment;
+import net.minecraft.client.model.geom.ModelLayerLocation;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.geom.PartPose;
+import net.minecraft.client.model.geom.builders.*;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.resources.model.Material;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.inventory.InventoryMenu;
+
+import java.util.function.Function;
+
+public class GuardianEyeModel extends AugmentModel<GuardianEyeAugment> {
+    public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(ResourceLocation.fromNamespaceAndPath(Nautec.MODID, "guardian_eye"), "main");
+    public static final Material MATERIAL = new Material(
+            InventoryMenu.BLOCK_ATLAS, ResourceLocation.fromNamespaceAndPath(Nautec.MODID, "augments/guardian_eye")
+    );;
+    private final ModelPart main;
+
+    public GuardianEyeModel(ModelPart root) {
+        super(root, RenderType::entitySolid);
+        this.main = root.getChild("main");
+    }
+
+    public static LayerDefinition createBodyLayer() {
+        MeshDefinition meshdefinition = new MeshDefinition();
+        PartDefinition partdefinition = meshdefinition.getRoot();
+
+        PartDefinition main = partdefinition.addOrReplaceChild("main", CubeListBuilder.create().texOffs(0, 0).addBox(-3.0F, -6.0F, -1.0F, 6.0F, 6.0F, 2.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, -6.0F));
+
+        return LayerDefinition.create(meshdefinition, 16, 16);
+    }
+
+    @Override
+    public void setupAnim(Player entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+
+    }
+
+    @Override
+    public void renderToBuffer(PoseStack poseStack, VertexConsumer buffer, int packedLight, int packedOverlay, int color) {
+        main.render(poseStack, buffer, packedLight, packedOverlay);
+    }
+}
