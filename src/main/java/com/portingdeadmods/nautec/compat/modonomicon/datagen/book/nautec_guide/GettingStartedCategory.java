fe0146ffd7e58b657aa5514571196c94cbd0260c
thepigcat <benpospo@gmail.com>
2024-09-28 20:58:37 +0200

    more modonomicon stuffs

--- src/main/java/com/portingdeadmods/nautec/compat/modonomicon/datagen/book/nautec_guide/GettingStartedCategory.java
+++ src/main/java/com/portingdeadmods/nautec/compat/modonomicon/datagen/book/nautec_guide/GettingStartedCategory.java
@@ -2,6 +2,7 @@ package com.portingdeadmods.nautec.compat.modonomicon.datagen.book.nautec_guide;
 
 import com.klikli_dev.modonomicon.api.datagen.CategoryProvider;
 import com.klikli_dev.modonomicon.api.datagen.ModonomiconProviderBase;
+import com.klikli_dev.modonomicon.api.datagen.book.BookEntryModel;
 import com.klikli_dev.modonomicon.api.datagen.book.BookIconModel;
 import com.portingdeadmods.nautec.compat.modonomicon.datagen.book.nautec_guide.getting_started.*;
 import com.portingdeadmods.nautec.registries.NTBlocks;
@@ -20,12 +21,24 @@ public class GettingStartedCategory extends CategoryProvider {
 
     @Override
     protected void generateEntries() {
-        add(new IntroductionEntry(this).generate(new Vec2(0, 0)));
-        add(new StructuresEntry(this).generate(new Vec2(2, 0)));
-        add(new LaserPowerEntry(this).generate(new Vec2(0, 2)));
-        add(new ItemTransformationEntry(this).generate(new Vec2(4, 0)));
-        add(new UtilsEntry(this).generate(new Vec2(6, 0)));
-        add(new MonocleEntry(this).generate(new Vec2(6, -2)));
+        BookEntryModel introductionEntry = new IntroductionEntry(this)
+                .generate(new Vec2(0, 0));
+        add(introductionEntry);
+        BookEntryModel structuresEntry = new StructuresEntry(this)
+                .generate(new Vec2(2, 0));
+        add(structuresEntry.withParent(introductionEntry));
+        BookEntryModel laserPowerEntry = new LaserPowerEntry(this)
+                .generate(new Vec2(4, 0));
+        add(laserPowerEntry.withParent(structuresEntry));
+        BookEntryModel itemTransformationEntry = new ItemTransformationEntry(this)
+                .generate(new Vec2(6, 0));
+        add(itemTransformationEntry.withParent(laserPowerEntry));
+        BookEntryModel utilsEntry = new UtilsEntry(this)
+                .generate(new Vec2(0, -2));
+        add(utilsEntry.withParent(introductionEntry));
+        BookEntryModel monocleEntry = new MonocleEntry(this)
+                .generate(new Vec2(6, 2));
+        add(monocleEntry.withParent(itemTransformationEntry));
     }
 
     @Override
--- src/main/java/com/portingdeadmods/nautec/compat/modonomicon/datagen/book/nautec_guide/LaserChemistryCategory.java
+++ src/main/java/com/portingdeadmods/nautec/compat/modonomicon/datagen/book/nautec_guide/LaserChemistryCategory.java
@@ -3,6 +3,7 @@ package com.portingdeadmods.nautec.compat.modonomicon.datagen.book.nautec_guide;
 import com.klikli_dev.modonomicon.api.datagen.CategoryProvider;
 import com.klikli_dev.modonomicon.api.datagen.ModonomiconProviderBase;
 import com.klikli_dev.modonomicon.api.datagen.book.BookIconModel;
+import com.portingdeadmods.nautec.registries.NTItems;
 
 public class LaserChemistryCategory extends CategoryProvider {
     public LaserChemistryCategory(ModonomiconProviderBase parent) {
@@ -21,16 +22,16 @@ public class LaserChemistryCategory extends CategoryProvider {
 
     @Override
     protected String categoryName() {
-        return "";
+        return "Laser Chemistry";
     }
 
     @Override
     protected BookIconModel categoryIcon() {
-        return null;
+        return BookIconModel.create(NTItems.ELECTROLYTE_ALGAE_SERUM_VIAL);
     }
 
     @Override
     public String categoryId() {
-        return "";
+        return "laser_chemistry";
     }
 }
