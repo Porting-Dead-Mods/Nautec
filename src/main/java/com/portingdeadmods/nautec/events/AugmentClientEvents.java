95e33957373c30c3f2ca5d21022f3f6eb983283d
ktpatient <kit.t.patient@gmail.com>
2024-12-06 20:53:59 +0000

    Change guardian eye rendering, add config to disable

--- src/main/java/com/portingdeadmods/nautec/events/AugmentClientEvents.java
+++ src/main/java/com/portingdeadmods/nautec/events/AugmentClientEvents.java
@@ -1,22 +1,20 @@
 package com.portingdeadmods.nautec.events;
 
+import com.portingdeadmods.nautec.NTConfig;
 import com.portingdeadmods.nautec.Nautec;
 import com.portingdeadmods.nautec.api.augments.Augment;
 import com.portingdeadmods.nautec.api.augments.AugmentSlot;
-import com.portingdeadmods.nautec.api.client.renderer.augments.ModelPartGetter;
 import com.portingdeadmods.nautec.client.renderer.augments.helper.GuardianEyeRenderHelper;
 import com.portingdeadmods.nautec.content.augments.GuardianEyeAugment;
 import com.portingdeadmods.nautec.events.helper.AugmentLayerRenderer;
 import com.portingdeadmods.nautec.events.helper.AugmentSlotsRenderer;
 import com.portingdeadmods.nautec.utils.AugmentHelper;
-import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.entity.player.PlayerRenderer;
 import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.client.event.RenderPlayerEvent;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @EventBusSubscriber(modid = Nautec.MODID, value = Dist.CLIENT)
@@ -30,6 +28,7 @@ public final class AugmentClientEvents {
 
     @SubscribeEvent
     public static void onRenderPlayer(RenderPlayerEvent.Post event) {
+        if (!NTConfig.allowAugmentRendering) return;
         PlayerRenderer renderer = event.getRenderer();
         renderer.addLayer(new AugmentLayerRenderer(renderer));
         // Needs to be outside the augment renderer cuz pose stacks
