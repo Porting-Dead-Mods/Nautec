f0c47530da206c3e0f593db48ebea697725001e6
Reclipse <17797013+ReclipseTheOne@users.noreply.github.com>
2024-12-14 20:04:00 +0200

    God may forgive what I've created here

--- src/main/java/com/portingdeadmods/nautec/NTRegistries.java
+++ src/main/java/com/portingdeadmods/nautec/NTRegistries.java
@@ -3,17 +3,30 @@ package com.portingdeadmods.nautec;
 import com.portingdeadmods.nautec.api.augments.AugmentSlot;
 import com.portingdeadmods.nautec.api.augments.AugmentType;
 import com.portingdeadmods.nautec.api.multiblocks.Multiblock;
+import com.portingdeadmods.nautec.bacteria.Bacteria;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.neoforge.registries.DataPackRegistryEvent;
 import net.neoforged.neoforge.registries.RegistryBuilder;
 
 public final class NTRegistries {
     private static final ResourceKey<Registry<AugmentType<?>>> AUGMENT_TYPE_KEY = ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(Nautec.MODID, "augment_type"));
     public static final ResourceKey<Registry<AugmentSlot>> AUGMENT_SLOT_KEY = ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(Nautec.MODID, "augment_slot"));
     private static final ResourceKey<Registry<Multiblock>> MULTIBLOCK_KEY = ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(Nautec.MODID, "multiblock"));
+    public static final ResourceKey<Registry<Bacteria>> BACTERIA_KEY = ResourceKey.createRegistryKey(ResourceLocation.fromNamespaceAndPath(Nautec.MODID, "bacteria"));
 
     public static final Registry<AugmentType<?>> AUGMENT_TYPE = new RegistryBuilder<>(AUGMENT_TYPE_KEY).create();
     public static final Registry<AugmentSlot> AUGMENT_SLOT = new RegistryBuilder<>(AUGMENT_SLOT_KEY).create();
     public static final Registry<Multiblock> MULTIBLOCK = new RegistryBuilder<>(MULTIBLOCK_KEY).create();
+
+    @SubscribeEvent
+    public static void registerDatapackRegistries(DataPackRegistryEvent.NewRegistry event) {
+        event.dataPackRegistry(
+                BACTERIA_KEY,
+                Bacteria.CODEC,
+                Bacteria.CODEC
+        );
+    }
 }
--- src/main/java/com/portingdeadmods/nautec/bacteria/Bacteria.java
+++ src/main/java/com/portingdeadmods/nautec/bacteria/Bacteria.java
@@ -1,5 +1,35 @@
 package com.portingdeadmods.nautec.bacteria;
 
-public interface Bacteria {
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.MapCodec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+import com.portingdeadmods.nautec.utils.codec.CodecUtils;
+import net.minecraft.world.item.Item;
 
+public interface Bacteria {
+    /*
+    Bacteria! We begin with only one!
+    Bacteria! Two's what we then become!
+    Bacteria! Each of us becomes two more!
+    Bacteria! We are stronger than before!
+    Bacteria! We keep growing at this rate!
+    Bacteria! No longer shall we wait!
+    Bacteria! The plan now unfolds!
+    Bacteria! We will take over the world!
+     */
+    Item type();
+    float growthRate();
+    float mutationResistance();
+    float productionRate();
+    int lifespan();
+
+    Codec<Bacteria> CODEC = RecordCodecBuilder.create(
+            instance -> instance.group(
+                    CodecUtils.ITEM_CODEC.fieldOf("type").forGetter(Bacteria::type),
+                    Codec.FLOAT.fieldOf("growth_rate").forGetter(Bacteria::growthRate),
+                    Codec.FLOAT.fieldOf("mutation_resistance").forGetter(Bacteria::mutationResistance),
+                    Codec.FLOAT.fieldOf("production_rate").forGetter(Bacteria::productionRate),
+                    Codec.INT.fieldOf("lifespan").forGetter(Bacteria::lifespan)
+            ).apply(instance, BacteriaImpl::new)
+    );
 }
