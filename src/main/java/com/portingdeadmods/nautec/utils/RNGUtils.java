d1a24b5732504d276be91fac2986b47e8402c79f
Reclipse <17797013+ReclipseTheOne@users.noreply.github.com>
2024-12-16 12:23:07 +0200

    Biased RNG

--- src/main/java/com/portingdeadmods/nautec/utils/RNGUtils.java
+++ src/main/java/com/portingdeadmods/nautec/utils/RNGUtils.java
@@ -1,35 +1,55 @@
 package com.portingdeadmods.nautec.utils;
 
 public final class RNGUtils {
-    public int uniformRandInt(int min, int max) {
+    public static int uniformRandInt(int min, int max) {
         return (int) (Math.random() * (max - min + 1) + min);
     }
 
-    public int uniformRandInt(int max) {
+    public static int uniformRandInt(int max) {
         return uniformRandInt(0, max);
     }
 
-    public double uniformRandDouble(double min, double max) {
+    public static double uniformRandDouble(double min, double max) {
         return Math.random() * (max - min) + min;
     }
 
-    public double uniformRandDouble(double max) {
+    public static double uniformRandDouble(double max) {
         return uniformRandDouble(0, max);
     }
 
-    public int intInRangeOf(int val, int range) {
+    public static int intInRangeOf(int val, int range) {
         return (int) (uniformRandInt(-val, val) * Math.random() + range);
     }
 
-    public int intInRangeOf(int val) {
+    public static int intInRangeOf(int val) {
         return intInRangeOf(val, 0);
     }
 
-    public double doubleInRangeOf(double val, double range) {
+    public static double doubleInRangeOf(double val, double range) {
         return uniformRandDouble(-val, val) * Math.random() + range;
     }
 
-    public double doubleInRangeOf(double val) {
+    public static double doubleInRangeOf(double val) {
         return doubleInRangeOf(val, 0);
+    }
+
+    /*
+    * -1 -> always in the negative range
+    * 1 -> always in the positive range
+    * 0 -> neutral
+     */
+    public static double biasedInRangeOf(double val, double range, double bias) {
+        double absBias = Math.abs(bias);
+
+        if (bias < 0) {
+            return uniformRandDouble(-range, range * (1 - absBias)) + val;
+        } else if (bias > 0) {
+            return uniformRandDouble(-range * (1 - absBias), range) + val;
+        }
+        return uniformRandDouble(-range, range) + val;
+    }
+
+    public static double biasedInRangeOf(double range, double bias) {
+        return biasedInRangeOf(0, range, bias);
     }
 }
