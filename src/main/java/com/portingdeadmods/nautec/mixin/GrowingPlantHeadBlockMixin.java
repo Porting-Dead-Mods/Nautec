26ba5a357d5db425b8a90c0fdf5fa3516fd178fd
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-02-20 20:06:07 +0100

    fix kelp block height config

--- src/main/java/com/portingdeadmods/nautec/mixin/GrowingPlantHeadBlockMixin.java
+++ src/main/java/com/portingdeadmods/nautec/mixin/GrowingPlantHeadBlockMixin.java
@@ -1,72 +1,22 @@
 package com.portingdeadmods.nautec.mixin;
 
 import com.portingdeadmods.nautec.NTConfig;
-import net.minecraft.core.BlockPos;
-import net.minecraft.core.Direction;
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.block.GrowingPlantHeadBlock;
 import net.minecraft.world.level.block.KelpBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(GrowingPlantHeadBlock.class)
-public abstract class GrowingPlantHeadBlockMixin {
-    @Shadow
-    protected abstract boolean isMaxAge(BlockState state);
-
-    @Shadow
-    protected abstract BlockState getMaxAgeState(BlockState state);
-
-    @Shadow
-    protected abstract int getBlocksToGrowWhenBonemealed(RandomSource random);
-
-    @Shadow
-    protected abstract boolean canGrowInto(BlockState state);
-
-    @Shadow
-    protected abstract BlockState getGrowIntoState(BlockState state, RandomSource random);
-
-    @Shadow @Final private double growPerTickProbability;
-
-    /**
-     * @author Leclowndu93150
-     * @reason because
-     */
-    @Overwrite
-    protected boolean isRandomlyTicking(BlockState state) {
-        if (state.getBlock() instanceof KelpBlock) {
-            return state.getValue(GrowingPlantHeadBlock.AGE) < NTConfig.kelpHeight;
-        }
-        return state.getValue(GrowingPlantHeadBlock.AGE) < GrowingPlantHeadBlock.MAX_AGE; // Default behavior for other plants
-    }
-
-    /**
-     * @author Leclowndu93150
-     * @reason maybe
-     */
-    @Overwrite
-    protected void randomTick(BlockState state, ServerLevel level, BlockPos pos, RandomSource random) {
-        if (state.getBlock() instanceof KelpBlock) {
-            if (state.getValue(GrowingPlantHeadBlock.AGE) < NTConfig.kelpHeight && net.neoforged.neoforge.common.CommonHooks.canCropGrow(level, pos.relative(Direction.UP), state, random.nextDouble() < this.growPerTickProbability)) {
-                BlockPos blockpos = pos.relative(Direction.UP);
-                if (this.canGrowInto(level.getBlockState(blockpos))) {
-                    level.setBlockAndUpdate(blockpos, this.getGrowIntoState(state, level.random));
-                    net.neoforged.neoforge.common.CommonHooks.fireCropGrowPost(level, blockpos, level.getBlockState(blockpos));
-                }
-            }
-        } else {
-            // Original behavior for other blocks
-            if (state.getValue(GrowingPlantHeadBlock.AGE) < 25 && net.neoforged.neoforge.common.CommonHooks.canCropGrow(level, pos.relative(Direction.UP), state, random.nextDouble() < this.growPerTickProbability)) {
-                BlockPos blockpos = pos.relative(Direction.UP);
-                if (this.canGrowInto(level.getBlockState(blockpos))) {
-                    level.setBlockAndUpdate(blockpos, this.getGrowIntoState(state, level.random));
-                    net.neoforged.neoforge.common.CommonHooks.fireCropGrowPost(level, blockpos, level.getBlockState(blockpos));
-                }
-            }
+public class GrowingPlantHeadBlockMixin {
+    @Inject(method = "getStateForPlacement", at = @At("RETURN"), cancellable = true)
+    private void onGetStateForPlacement(LevelAccessor level, CallbackInfoReturnable<BlockState> cir) {
+        BlockState state = cir.getReturnValue();
+        if(state != null && state.getBlock() instanceof KelpBlock) {
+            cir.setReturnValue(state.setValue(GrowingPlantHeadBlock.AGE, level.getRandom().nextInt(NTConfig.kelpHeight)));
         }
     }
 }
