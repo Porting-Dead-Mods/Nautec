90da0f9c3b66ebf6acfa31174d6feda3e434ec2f
ktpatient <kit.t.patient@gmail.com>
2025-02-28 18:01:19 +0000

    Fix bucketing out of MixerBlock

--- src/main/java/com/portingdeadmods/nautec/content/menus/MixerMenu.java
+++ src/main/java/com/portingdeadmods/nautec/content/menus/MixerMenu.java
@@ -9,6 +9,7 @@ import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.CauldronBlock;
 import net.neoforged.neoforge.items.SlotItemHandler;
 import org.jetbrains.annotations.NotNull;
 
