94f1add132f49ef24c3b0fa4fb723962f092e5d7
Leclowndu93150 <xdr.a123454321@gmail.com>
2025-10-27 10:06:05 +0100

    fix mixer bucket interactions

--- src/main/java/com/portingdeadmods/nautec/content/blocks/MixerBlock.java
+++ src/main/java/com/portingdeadmods/nautec/content/blocks/MixerBlock.java
@@ -1,25 +1,17 @@
 package com.portingdeadmods.nautec.content.blocks;
 
 import com.mojang.serialization.MapCodec;
-import com.portingdeadmods.nautec.Nautec;
 import com.portingdeadmods.nautec.api.blockentities.ContainerBlockEntity;
 import com.portingdeadmods.nautec.api.blocks.blockentities.LaserBlock;
 import com.portingdeadmods.nautec.api.utils.HorizontalDirection;
 import com.portingdeadmods.nautec.content.blockentities.MixerBlockEntity;
 import com.portingdeadmods.nautec.registries.NTBlockEntityTypes;
-import com.portingdeadmods.nautec.utils.ItemUtils;
-import net.minecraft.ChatFormatting;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.network.chat.Component;
-import net.minecraft.sounds.SoundEvents;
-import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.BucketItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
@@ -28,13 +20,12 @@ import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
-import net.minecraft.world.level.material.Fluids;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.neoforged.neoforge.capabilities.Capabilities;
-import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.fluids.FluidUtil;
 import net.neoforged.neoforge.fluids.capability.IFluidHandler;
 import net.neoforged.neoforge.fluids.capability.templates.FluidTank;
 import net.neoforged.neoforge.items.IItemHandler;
@@ -90,57 +81,17 @@ public class MixerBlock extends LaserBlock {
     @Override
     protected @NotNull ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hitResult) {
         if (level.getBlockEntity(pos) instanceof MixerBlockEntity mixerBE) {
-            IFluidHandler fluidHandler = mixerBE.getFluidHandler();
-
             if (stack.getCapability(Capabilities.FluidHandler.ITEM) != null) {
-                IFluidHandler itemFluidHandler = stack.getCapability(Capabilities.FluidHandler.ITEM);
                 if (mixerBE.getFluidHandler() instanceof FluidTank fluidTank && mixerBE.getSecondaryFluidHandler() instanceof FluidTank secFluidTank) {
-                    if (itemFluidHandler.getFluidInTank(0).isEmpty()) {
-                        extractFluid(player, level, hand, fluidTank, secFluidTank, itemFluidHandler);
-                    } else {
-                        insertFluid(player, level, hand, fluidHandler, itemFluidHandler);
+                    FluidTank targetTank = secFluidTank.getFluidInTank(0).isEmpty() ? fluidTank : secFluidTank;
+                    
+                    if (FluidUtil.interactWithFluidHandler(player, hand, targetTank)) {
+                        return ItemInteractionResult.SUCCESS;
                     }
-                    return ItemInteractionResult.SUCCESS;
                 }
             }
         }
         return super.useItemOn(stack, state, level, pos, player, hand, hitResult);
-    }
-
-    private static void insertFluid(Player player, Level level, InteractionHand interactionHand, IFluidHandler fluidHandler, IFluidHandler fluidHandlerItem) {
-        int filled = fluidHandler.fill(fluidHandlerItem.getFluidInTank(0).copy(), IFluidHandler.FluidAction.EXECUTE);
-        FluidStack drained = fluidHandlerItem.drain(filled, IFluidHandler.FluidAction.EXECUTE);
-        if (player.getItemInHand(interactionHand).getItem() instanceof BucketItem bucketItem && !drained.isEmpty() && drained.getAmount() >= 1000) {
-            player.getItemInHand(interactionHand).shrink(1);
-            ItemUtils.giveItemToPlayerNoSound(player, Items.BUCKET.getDefaultInstance());
-            if (bucketItem.content.isSame(Fluids.WATER)) {
-                level.playSound(null, player.getX(), player.getY() + 0.5, player.getZ(), SoundEvents.BUCKET_EMPTY, SoundSource.PLAYERS, 0.8F, 1.0F);
-            } else if (bucketItem.content.isSame(Fluids.LAVA)) {
-                level.playSound(null, player.getX(), player.getY() + 0.5, player.getZ(), SoundEvents.BUCKET_EMPTY_LAVA, SoundSource.PLAYERS, 0.8F, 1.0F);
-            }
-        }
-    }
-
-    private static void extractFluid(Player player, Level level, InteractionHand interactionHand, FluidTank fluidHandler, FluidTank secondaryFluidHandler, IFluidHandler fluidHandlerItem) {
-        FluidTank curHandler = secondaryFluidHandler.getFluidInTank(0).isEmpty() ? fluidHandler : secondaryFluidHandler;
-        // Nautec.LOGGER.debug("Extracting secondary: {}", curHandler == secondaryFluidHandler);
-        FluidStack fluidInTank = curHandler.getFluidInTank(0);
-        if (player.getItemInHand(interactionHand).is(Items.BUCKET) && !fluidInTank.isEmpty()) {
-            player.getItemInHand(interactionHand).shrink(1);
-            ItemUtils.giveItemToPlayerNoSound(player, fluidInTank.getFluid().getBucket().getDefaultInstance());
-            if (fluidInTank.is(Fluids.WATER)) {
-                level.playSound(null, player.getX(), player.getY() + 0.5, player.getZ(), SoundEvents.BUCKET_FILL, SoundSource.PLAYERS, 0.8F, 1.0F);
-            } else if (fluidInTank.is(Fluids.LAVA)) {
-                level.playSound(null, player.getX(), player.getY() + 0.5, player.getZ(), SoundEvents.BUCKET_FILL_LAVA, SoundSource.PLAYERS, 0.8F, 1.0F);
-            }
-            curHandler.drain(1000, IFluidHandler.FluidAction.EXECUTE);
-        } else {
-            FluidStack fluidStack = curHandler.drain(curHandler.getFluidInTank(0).getAmount(), IFluidHandler.FluidAction.EXECUTE);
-            int remainderAmount = fluidHandlerItem.fill(fluidStack, IFluidHandler.FluidAction.EXECUTE);
-            FluidStack newFluidStack = fluidStack.copy();
-            newFluidStack.setAmount(remainderAmount);
-            curHandler.setFluid(newFluidStack);
-        }
     }
 
     private ItemInteractionResult insertItemsSided(ItemStack stack, Player player, InteractionHand hand, IItemHandler itemHandler, Direction clickedFace) {
